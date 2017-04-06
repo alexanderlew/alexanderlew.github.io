d88c0fbe607dde45d0bea2cb0c790e24aea8787b
alexanderlew <alexanderclew@gmail.com>
2017-04-05 21:36:05 -0700

    mbta

---
+++ projects/mbta/js/mapscripts.js
@@ -0,0 +1,190 @@
+var map;
+
+var infowindow;
+
+//time between position updates
+var time = 15000;
+
+// markers for map
+var markers = [];
+
+var styles= [
+       
+		//desaturate all others to fade in background.
+		{
+			featureType: "administrative",
+			
+			stylers: [
+			{saturation:-20},
+			{hue: '#00ffe6'} 
+			]
+		},{
+			featureType: "landscape",
+	
+			
+			stylers: [
+			{saturation:-20},
+			{hue: '#00ffe6'} 
+			]
+		},
+		{
+			featureType: "landscape",
+			
+			stylers: [
+			{saturation:-20},
+			{hue: '#00ffe6'} 
+			]
+		}, {
+			featureType: "poi",
+			
+			stylers: [
+			{saturation:-20},
+			{hue: '#00ffe6'} 
+			]
+		},{
+			featureType: "road",
+			
+			stylers: [
+			{saturation:-20},
+			{hue: '#00ffe6'} 
+			]
+		}, {
+			featureType: "water",
+			
+			stylers: [
+			{saturation:-20},
+			{hue: '#00ffe6'} 
+			]
+		}
+		
+
+    ];
+    
+    
+    
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('station-map'), {
+      center: {lat: 42.3581756, lng: -71.0581317},
+      zoom: 15,
+      styles: styles
+    });
+    
+    var transitLayer = new google.maps.TransitLayer();
+        transitLayer.setMap(map);
+
+    infowindow = new google.maps.InfoWindow();
+
+	configure()
+}
+
+function addTrains(Line){
+    var url = 'http://realtime.mbta.com/developer/api/v2/vehiclesbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=' + Line + '&format=JSON';
+    
+	//retrieve JSON of locations
+	$.getJSON(url).done(function(data, textStatus, jqHXR){
+		
+		//Iterate through each direction
+		for(var i = 0;  i < data.direction.length; i++){
+			
+			//Iterate through each trip.
+			for(var j = 0; j < data.direction[i].trip.length; j++){
+				
+				var lat = data.direction[i].trip[j].vehicle.vehicle_lat;
+				var long = data.direction[i].trip[j].vehicle.vehicle_lon;
+				var bearing = Number(data.direction[i].trip[j].vehicle.vehicle_bearing);
+				var dest = data.direction[i].trip[j].trip_headsign;
+				
+				var LatLng = new google.maps.LatLng(lat,long);
+				
+				
+				//Define symbol + color based off what line we are requesting.
+	            var color;
+				//define color based on line.
+				if(Line == "Blue"){
+				color = "#2f5da6";
+				}
+				else if(Line == "Green-B" || Line == "Green-C" || Line =="Green-D" || Line == "Green-E"){
+				color = "#008150";
+				}
+				else if(Line == "Orange"){
+				color = "#fd8a03";
+				}
+				else if(Line == "Red"){
+				color = "#fa2d27";
+				}
+				
+				//define custom icon
+				var vehicle_icon = {
+				path: 'M 5,3.8 5.1,19.5 2.7,16.5 0.3,19.5 0.3,3.8 2.6,0.4 z',
+				fillColor: color,
+				fillOpacity: 1,
+				strokeColor: '#4c4c4c',
+				rotation: bearing,
+				scale: 1.5
+				};
+				
+				//Create marker
+				
+				var marker = new MarkerWithLabel({
+				position: LatLng,
+				map: map,
+				icon: vehicle_icon,
+				labelClass: "labels",
+				labelContent: dest,
+				
+				});
+				
+				//place markers on map
+				marker.setMap(map);
+				
+				//update the global marker array:
+				markers.push(marker);
+            
+					
+			}
+		}
+	
+	});
+    
+    
+    
+}    
+	  
+
+// Function to remove trains
+function removeTrains() {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(null);
+	}
+	markers = [];
+}
+	
+
+function configure()
+{
+	//First add all trains
+	addTrains("Blue");
+	addTrains("Green-B");
+	addTrains("Green-C");
+	addTrains("Green-D");
+	addTrains("Green-E");
+    addTrains("Orange");
+    addTrains("Red");
+    
+    //then run every 10 seconds, first removing, then adding markers again with 
+    // new locations.
+	setInterval(function(){
+	//First remove any markers if they exist.
+	removeTrains();
+	
+	//Add trains back in with new locations.
+	addTrains("Blue");
+	addTrains("Green-B");
+	addTrains("Green-C");
+	addTrains("Green-D");
+	addTrains("Green-E");
+    addTrains("Orange");
+    addTrains("Red");
+	}, time);
+}
